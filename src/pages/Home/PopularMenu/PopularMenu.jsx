b0bad5de79c09b30854b3834bbec1f547945869e
Your Name <you@example.com>
2023-12-29 10:33:08 +0600

    menu

--- src/pages/Home/PopularMenu/PopularMenu.jsx
+++ src/pages/Home/PopularMenu/PopularMenu.jsx
@@ -1,18 +1,21 @@
 
-import { useEffect, useState } from 'react';
 import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 import MenuItem from '../../Share/MenuItem/MenuItem';
+import useMenu from '../../../hooks/useMenu';
 
 const PopularMenu = () => {
-    const [menu, setMenu] = useState([]);
-    useEffect(() => {
-        fetch('menu.json')
-            .then(res => res.json())
-            .then(data => {
-                const popularItems = data.filter(item => item.category === 'popular')
-                setMenu(popularItems)
-            })
-    }, [])
+    const [menu] = useMenu();
+    const popular = menu.filter(item => item.category === 'popular')
+    // or
+    // const [menu, setMenu] = useState([]);
+    // useEffect(() => {
+    //     fetch('menu.json')
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             const popularItems = data.filter(item => item.category === 'popular')
+    //             setMenu(popularItems)
+    //         })
+    // }, [])
     return (
         <section className='my-8'>
             <SectionTitle
@@ -22,7 +25,7 @@ const PopularMenu = () => {
             </SectionTitle>
             <div className='grid md:grid-cols-2 gap-6'>
                 {
-                    menu.map(item => <MenuItem
+                    popular.map(item => <MenuItem
                         key={item._id}
                         item={item}
                     ></MenuItem>)
--- src/pages/Menu/Menu/Menu.jsx
+++ src/pages/Menu/Menu/Menu.jsx
@@ -1,18 +1,53 @@
 import { Helmet } from 'react-helmet-async';
 import Cover from '../../Share/Cover/Cover';
 import menuImg from '../../../assets/menu/banner3.jpg'
-import PopularMenu from '../../Home/PopularMenu/PopularMenu';
-const Menu = () => {
+import dessert from '../../../assets/menu/dessert-bg.jpeg'
+import soupImg from '../../../assets/menu/soup-bg.jpg'
+import saladImg from '../../../assets/menu/salad-bg.jpg'
+import pizzaImg from '../../../assets/menu/pizza-bg.jpg'
+import useMenu from '../../../hooks/useMenu';
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
+import MenuCategory from '../MenuCategory/MenuCategory';
 
+const Menu = () => {
+    const [menu] = useMenu();
+    const desserts = menu.filter(item => item.category === 'dessert')
+    const soup = menu.filter(item => item.category === 'soup')
+    const salad = menu.filter(item => item.category === 'salad')
+    const pizza = menu.filter(item => item.category === 'pizza')
+    const offered = menu.filter(item => item.category === 'offered')
     return (
         <div>
             <Helmet> <title>HERO RESTAURANT | MENU</title></Helmet>
+            {/* main  */}
             <Cover img={menuImg} title='Our Menu'></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title='Our Menu'></Cover>
-            <PopularMenu></PopularMenu>
-            <Cover img={menuImg} title='Our Menu'></Cover>
-            <PopularMenu></PopularMenu>
+            <SectionTitle subHeading="Don't Miss" heading="Today's Offer"></SectionTitle>
+            {/* offered */}
+            <MenuCategory items={offered}></MenuCategory>
+            {/* desserts */}
+            <MenuCategory
+                items={desserts}
+                img={dessert}
+                title={'Dessert'}
+            ></MenuCategory>
+            {/* soup */}
+            <MenuCategory
+                items={soup}
+                img={soupImg}
+                title={'Soup'}
+            ></MenuCategory>
+            {/* salad */}
+            <MenuCategory
+                items={salad}
+                img={saladImg}
+                title={'Salad'}
+            ></MenuCategory>
+            {/* pizza */}
+            <MenuCategory
+                items={pizza}
+                img={pizzaImg}
+                title={'Pizza'}
+            ></MenuCategory>
         </div>
     );
 };
