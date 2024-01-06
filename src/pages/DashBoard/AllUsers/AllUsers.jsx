16af668f6862fd1c4a5d3545a30f7d8c36fac01b
Your Name <you@example.com>
2024-01-06 07:28:21 +0600

    verify token

--- src/pages/DashBoard/AllUsers/AllUsers.jsx
+++ src/pages/DashBoard/AllUsers/AllUsers.jsx
@@ -4,21 +4,40 @@ import { CiTrash, CiUser } from "react-icons/ci";
 import Swal from "sweetalert2";
 
 
+
+
 const AllUsers = () => {
     const axiosSecure = useAxios();
     const { data: users = [], refetch } = useQuery({
         queryKey: ['users'],
         queryFn: async () => {
             const res = await axiosSecure.get('/users');
+            // console.log('all users', res.data)
             return res.data
+
         }
     })
 
-    const handleMakeAdmin = () => {
-
+    const handleMakeAdmin = user => {
+        axiosSecure.patch(`/users/${user._id}`)
+            .then(res => {
+                if (res.data.modifiedCount > 0) {
+                    refetch()
+                    Swal.fire({
+                        position: "top-end",
+                        icon: "success",
+                        title: `${user.name} is an admin now`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    });
+                }
+            })
     }
 
-    const handleDelete = user => {
+
+
+
+    const handleDeleteUser = user => {
         Swal.fire({
             title: "Are you sure?",
             text: "You won't be able to revert this!",
@@ -67,15 +86,15 @@ const AllUsers = () => {
                             {users.map((user, index) => <tr key={user._id}>
                                 <th>{index + 1}</th>
                                 <td>{user.name}</td>
-                                <td>user.email</td>
+                                <td>{user.email}</td>
                                 <td>
-                                    <button
-                                        onClick={() => handleMakeAdmin(user._id)}
-                                        className="btn btn-ghost btn-xs text-lg"><CiUser className='text-red-900'></CiUser></button>
+                                    {user.role === 'admin' ? 'admin' : <button
+                                        onClick={() => handleMakeAdmin(user)}
+                                        className="btn btn-ghost btn-xs text-lg"><CiUser className='text-red-900'></CiUser></button>}
                                 </td>
                                 <td>
                                     <button
-                                        onClick={() => handleDelete(user._id)}
+                                        onClick={() => handleDeleteUser(user)}
                                         className="btn btn-ghost btn-xs text-lg"><CiTrash className='text-red-900'></CiTrash></button>
                                 </td>
                             </tr>)}
--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -1,11 +1,14 @@
 import { createContext, useEffect, useState } from "react";
 import { GoogleAuthProvider, createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
 import app from "../firebase/firebase.config";
+import useAxiosPublic from './../hooks/useAxiosPublic';
+
 
 export const AuthContext = createContext(null)
 const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
+    const axiosPublic = useAxiosPublic();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const googleProvider = new GoogleAuthProvider();
@@ -39,6 +42,19 @@ const AuthProvider = ({ children }) => {
         const unsubscribe = onAuthStateChanged(auth, currentUser => {
             setUser(currentUser);
             console.log('current user', currentUser);
+            if (currentUser) {
+                // get token and store client
+                const userInfo = { email: currentUser.email }
+                axiosPublic.post('/jwt', userInfo)
+                    .then(res => {
+                        if (res.data.token) {
+                            localStorage.setItem('access-token', res.data.token)
+                        }
+                    })
+            }
+            else {
+                localStorage.removeItem('access-token')
+            }
             setLoading(false)
         })
         return () => {
